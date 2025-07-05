165d931c1070757cbd6630862c47f226d975d888
Martha Lacerda <martinha.lacerda@gmail.com>
2025-07-05 00:00:47 -0300

    Leitura do arquivo csv retornando lista de dicionarios em csv_repo.py

---
+++ src/models/pessoa.py
@@ -0,0 +1,27 @@
+"""Representação de Pessoa"""
+
+
+
+
+class Pessoa:
+    """Usuario proveniente da lista de clientes
+    
+    Atributos:
+        nome_completo (str): Nome completo da pessoa
+        primeiro_nome
+        segundo_nome
+        genero
+        email
+        celular
+        interesse (str): 
+        cpf (str): CEP da Pessoa 
+        bairro (str): Bairro do endereço da Pessoa
+        cidade (str): Cidade do endereço da pessoa
+        estado (str): Estado do endereço da Pessoa
+        observacoes (list[str]): Observações sobre inconsistências nos dados
+
+
+    """
+
+
+
---
+++ src/repo/csv_repo.py
@@ -0,0 +1,39 @@
+"""Leitura do arquivo csv com os dados"""
+
+
+import csv
+from pathlib import Path
+
+
+def ler_csv(caminho_arquivo: Path) -> list[dict]:
+    """Lê um arquivo csv e retorna seus dados sem tratamento
+    
+    Parâmetros:
+        caminho_arquivo: Path - caminho para o arquivo csv em ./data.
+    
+    Raises:
+        FileNotFoundError: Caso o arquivo não seja encontrado.
+    
+    Retorna:
+        list[dict] - lista de registros do csv, cada um como um dicionario.
+    """
+    
+    # Verificar se o caminho existe
+    if not caminho_arquivo.exists():
+        raise FileNotFoundError('Arquivo não encontrado.')
+
+    # Abrir o arquivo usando Path.open()
+    with caminho_arquivo.open(mode='r', encoding='utf-8') as arquivo:
+    
+        # O DictReader considera a 1a linha como cabeçalho, e depois cada linha como um dicionário.
+        leitor = csv.DictReader(arquivo)
+        return list(leitor)
+
+
+
+# Testando a leitura e acesso ao arquivo como lista
+if __name__ == '__main__':
+
+    lista = ler_csv(Path(__file__).resolve().parent.parent.parent / "data" / "lista_clientes.csv")
+    print(lista[0])
+    print(lista[49])
